0288a50daf87a35e902dc2ec9520ad0bd000b6d2
João Fernando Baraky <jfbaraky@gmail.com>
2017-04-25 22:41:24 -0300

    V1.0

---
+++ README.md
@@ -0,0 +1,30 @@
+# React Tutorial Pt 1: A Comprehensive Guide to Building Apps with React.js
+
+Implementation of [A Comprehensive Guide to Building Apps with React.js codelab](https://tylermcginnis.com/reactjs-tutorial-a-comprehensive-guide-to-building-apps-with-react/), a serie of tutorials for the first steps in React.
+
+The first part put together all the basic concepts behind the React and this is the final code of the tutorial.
+
+## How to Setup
+
+First you need:
+
+* [Node.js with npm](https://nodejs.org/en/)
+
+After you clone this code, you need to install some packages and start the app, so select the project folder in the cmd prompt:
+
+```
+$ npm install
+$ npm start
+```
+This will open your browser with the application.
+
+
+## Built with
+
+* [Visual Studio Code 1.11.2](https://code.visualstudio.com/)
+
+## Also see
+
+See the other parts of this tutorial:
+* [Part 1.5](https://github.com/jfbaraky/Building-Apps-with-React.js-PT.1.5-)
+* [Part 2](https://github.com/jfbaraky/Building-Apps-with-React.js-PT.2-)
---
+++ src/index.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+var FriendsContainer = React.createClass({
+  getInitialState: function() {
+    return {
+      name: 'João Fernando Baraky',
+      friends: ['James Hetfield', 'Lars Ulrich', 'Kirk Hammett'],
+    };
+  },
+  addFriend: function(friend) {
+    this.setState({
+      friends: this.state.friends.concat([friend]),
+    });
+  },
+  render: function() {
+    return (
+      <div>
+        <h3> Name: {this.state.name} </h3>
+        <AddFriend addNew={this.addFriend} />
+        <ShowList names={this.state.friends} />
+      </div>
+    );
+  },
+});
+var AddFriend = React.createClass({
+  getInitialState: function() {
+    return {
+      newFriend: '',
+    };
+  },
+  propTypes: {
+    addNew: React.PropTypes.func.isRequired,
+  },
+  updateNewFriend: function(e) {
+    this.setState({
+      newFriend: e.target.value,
+    });
+  },
+  handleAddNew: function() {
+    this.props.addNew(this.state.newFriend);
+    this.setState({
+      newFriend: '',
+    });
+  },
+  render: function() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.newFriend}
+          onChange={this.updateNewFriend}
+        />
+        <button onClick={this.handleAddNew}> Add Friend </button>
+      </div>
+    );
+  },
+});
+var ShowList = React.createClass({
+  getDefaultProps: function() {
+    return {
+      names: [],
+    };
+  },
+  render: function() {
+    var listItems = this.props.names.map(function(friend) {
+      return <li> {friend} </li>;
+    });
+    return (
+      <div>
+        <h3> Friends </h3>
+        <ul>
+          {listItems}
+        </ul>
+      </div>
+    );
+  },
+});
+
+render(<FriendsContainer />, document.getElementById('root'));
